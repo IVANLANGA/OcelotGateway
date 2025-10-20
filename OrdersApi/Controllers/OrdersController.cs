e71604e5791a415cdcd5f7dbc6fe2638097a1304
IVANLANGA <n.ivanlanga@icloud.com>
2025-10-20 14:07:44 +0200

    Added APId

---
+++ OrdersApi/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrdersApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    private static readonly object[] Data =
+    {
+        new { Id = 101, UserId = 1, Item = "Keyboard", Total = 899.00 },
+        new { Id = 102, UserId = 2, Item = "Mouse",    Total = 399.00 }
+    };
+
+    // GET /api/orders
+    [HttpGet]
+    public IActionResult GetAll() => Ok(Data);
+
+    // GET /api/orders/user/1
+    [HttpGet("user/{userId:int}")]
+    public IActionResult GetByUser(int userId)
+        => Ok(Data.Where(o => (int)o.GetType().GetProperty("UserId")!.GetValue(o)! == userId));
+}
---
+++ UsersApi/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsersApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private static readonly object[] Data =
+    {
+        new { Id = 1, Name = "Alice" },
+        new { Id = 2, Name = "Bob" }
+    };
+
+    // GET /api/users
+    [HttpGet]
+    public IActionResult GetAll() => Ok(Data);
+
+    // GET /api/users/1
+    [HttpGet("{id:int}")]
+    public IActionResult GetById(int id)
+    {
+        var user = Data.Cast<dynamic>().FirstOrDefault(u => (int)u.Id == id);
+        return user is null ? NotFound() : Ok(user);
+    }
+}
